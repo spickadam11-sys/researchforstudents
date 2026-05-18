7b070723123153877355d2fba92fb456ab960f20
researchforstudents.com <spick.adam11@gmail.com>
2026-05-17 21:40:40 -0400

    /

--- app/proxy/page.js
+++ app/proxy/page.js
@@ -12,7 +12,7 @@ export default function ProxyPage() {
       height: "calc(100vh - 58px)",
     }}>
       <iframe
-        src="incognito-production-7b03.up.railway.app/"
+        src="https://incognito-production-7b03.up.railway.app/"
         style={{
           width: "100%",
           height: "100%",
